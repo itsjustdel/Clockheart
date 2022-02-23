9deb627e0c16fb8b710ad739fcbb5842b5863253
Ross Brown <rossbrown22@hotmail.co.uk>
2022-02-23 10:20:26 +0000

    fixed id problem

--- ClockheartFrontEnd/app/src/components/ShopList.js
+++ ClockheartFrontEnd/app/src/components/ShopList.js
@@ -2,41 +2,30 @@ const ShopList = ({ updateItems, characters, items }) => {
     console.log("Shop list")
 
     const handleItemClick = (event) => {
-        const index = event.target.value;       
-        
+        const index = event.target.value
+
         //player is always first in character array (but has ID 1! Be careful!)
-        const newOwner = characters[0];
+        const newOwner = characters[0]
         const updatedShopItem = {
-            "name": items[ index ]["name"],
-            "value": items[ index ]["value"],
-            "damage": items[ index ]["damage"],
+            "name": items[index].name,
+            "value": items[index].value,
+            "damage": items[index].damage,
             "character": newOwner
         }
-        
-        updateItems(event.target.value, updatedShopItem);
-        
-        const updatedShopItemBE = {
-            "name": "Sword",
-            "value": 5,
-            "damage": 5,
-            "character": 
-                {
-                "id": 1
-                }
-            }
-            const str = '/items/1'
-            console.log(str)
-            fetch(str, {
-                method: 'PUT',
-                body: JSON.stringify(updatedShopItemBE),
-                headers: { 'Content-Type': 'application/json' }
-            })
-            .then(res => console.log( res.json()))
+        updateItems(event.target.value, updatedShopItem)
+
+        const str = `/items/${items[index].id}`
+        fetch(str, {
+            method: 'PUT',
+            body: JSON.stringify(updatedShopItem),
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(res => res.json())
     }
 
-    const itemsForSale = items.map((item, index) => {  
-        if(item.character.name == "Zebediah Flint")      
-            return <li onClick={handleItemClick} value={index} key ={index}>{item.name}</li>
+    const itemsForSale = items.map((item, index) => {
+        if (item.character.name == "Zebediah Flint")
+            return <li onClick={handleItemClick} value={index} key={index}>{item.name}</li>
 
     })
 
