6a0c0e86df7b21f69bd00d86c4e80f7ac39727bf
NaomiValuks <naomi@valuks.com>
2022-02-24 16:18:54 +0000

    added loot to all bosses

--- ClockheartBackend/src/main/java/com/example/ClockheartBackend/components/DataLoader.java
+++ ClockheartBackend/src/main/java/com/example/ClockheartBackend/components/DataLoader.java
@@ -33,6 +33,8 @@ public class DataLoader implements ApplicationRunner {
         QuestGiver questGiver = new QuestGiver("Cogsworth", 5, 5, 5);
         gameCharacterRepository.save(questGiver);
 
+        //BOSSES
+
         GameCharacter rubyBoss = new GameCharacter("Ruby Boss", 3, 5, 3);
         gameCharacterRepository.save(rubyBoss);
 
@@ -45,17 +47,21 @@ public class DataLoader implements ApplicationRunner {
         GameCharacter diamondBoss = new GameCharacter("Diamond Boss", 8, 5, 8);
         gameCharacterRepository.save(diamondBoss);
 
+        //QUESTS
+
         Quest rustAndDust = new Quest("Rust and Dust", 10, questGiver);
         questRepository.save(rustAndDust);
 
         Quest defeatDellyWelly = new Quest("Defeat Delly Welly", 18, questGiver);
         questRepository.save(defeatDellyWelly);
 
-        Quest rockPaperScissors = new Quest("Rock Paper Scissors", 20, questGiver);
-        questRepository.save(rockPaperScissors);
+        Quest rockPaperScrews = new Quest("Rock Paper Screws", 20, questGiver);
+        questRepository.save(rockPaperScrews);
 
         Quest clockTowerBar = new Quest("ClockTowerBar", 0, shop);
         questRepository.save(clockTowerBar);
+
+        //SHOP WEAPONS
 
         Attack woodenSword = new Attack("Wooden Sword", 4, shop, 3);
         itemRepository.save(woodenSword);
@@ -77,15 +83,17 @@ public class DataLoader implements ApplicationRunner {
 
         Attack diamondKnife = new Attack("Diamond Knife", 10, shop, 10);
         itemRepository.save(diamondKnife);
-        
-        Attack flinkLockPistol = new Attack("Flink Lock Pistol", 4, shop, 5);
-        itemRepository.save(flinkLockPistol);
+
+        Attack flintlockPistol = new Attack("Flintlock Pistol", 4, shop, 5);
+        itemRepository.save(flintlockPistol);
 
         Attack repeater = new Attack("Repeater", 10, shop, 11);
         itemRepository.save(repeater);
 
         Attack blunderbuss = new Attack("Blunderbuss", 19, shop, 21);
         itemRepository.save(blunderbuss);
+
+        //SHOP POTIONS
 
         Healing basicPotion = new Healing("Basic Health Potion", 6, shop, 6);
         itemRepository.save(basicPotion);
@@ -99,17 +107,124 @@ public class DataLoader implements ApplicationRunner {
         Healing sage = new Healing("Sage", 3, shop, 3);
         itemRepository.save(sage);
 
+        //RUBY ITEMS
+
         Item rubyGem = new Item("Ruby Gem", 0, rubyBoss);
         itemRepository.save(rubyGem);
+
+        Healing rubySagePotion = new Healing("Sage", 3, rubyBoss, 3);
+        itemRepository.save(rubySagePotion);
+
+        Healing rubyPotion = new Healing("Basic Health Potion", 6, rubyBoss, 6);
+        itemRepository.save(rubyPotion);
+
+        Healing rubyPotion2 = new Healing("Basic Health Potion", 6, rubyBoss, 6);
+        itemRepository.save(rubyPotion2);
+
+        Healing rubyPotion3 = new Healing("Average Health Potion", 10, rubyBoss, 10);
+        itemRepository.save(rubyPotion3);
+
+        Item littleMoneyBagRuby = new Item("Little Money Bag", 5, rubyBoss);
+        itemRepository.save(littleMoneyBagRuby);
+
+        Item moneyBagRuby = new Item("Basic Money Bag", 10, rubyBoss);
+        itemRepository.save(moneyBagRuby);
+
+        Item moneyBagRuby2 = new Item("Basic Money Bag", 10, rubyBoss);
+        itemRepository.save(moneyBagRuby2);
+
+        Item moneyBagRuby3 = new Item("Average Money Bag", 15, rubyBoss);
+        itemRepository.save(moneyBagRuby3);
+
+        //SAPPHIRE ITEMS
 
         Item sapphireGem = new Item("Sapphire Gem", 0, sapphireBoss);
         itemRepository.save(sapphireGem);
 
+        Healing sapphirePotion = new Healing("Basic Health Potion", 6, sapphireBoss, 6);
+        itemRepository.save(sapphirePotion);
+
+        Healing sapphirePotion2 = new Healing("Basic Health Potion", 6, sapphireBoss, 6);
+        itemRepository.save(sapphirePotion2);
+
+        Healing sapphirePotion3 = new Healing("Average Health Potion", 10, sapphireBoss, 10);
+        itemRepository.save(sapphirePotion3);
+
+        Healing sapphirePotion4 = new Healing("Average Health Potion", 10, sapphireBoss, 10);
+        itemRepository.save(sapphirePotion4);
+
+        Item littleMoneyBag = new Item("Little Money Bag", 5, sapphireBoss);
+        itemRepository.save(littleMoneyBag);
+
+        Item moneyBagSapphire = new Item("Basic Money Bag", 10, sapphireBoss);
+        itemRepository.save(moneyBagSapphire);
+
+        Item moneyBagSapphire2 = new Item("Average Money Bag", 15, sapphireBoss);
+        itemRepository.save(moneyBagSapphire2);
+
+        Item moneyBagSapphire3 = new Item("Average Money Bag", 15, sapphireBoss);
+        itemRepository.save(moneyBagSapphire3);
+
+
+        //EMERALD ITEMS
+
         Item emeraldGem = new Item("Emerald Gem", 0, emeraldBoss);
         itemRepository.save(emeraldGem);
 
+        Healing emeraldPotion = new Healing("Average Health Potion", 10, emeraldBoss, 10);
+        itemRepository.save(sapphirePotion);
+
+        Healing emeraldPotion2 = new Healing("Average Health Potion", 10, emeraldBoss, 10);
+        itemRepository.save(emeraldPotion2);
+
+        Healing emeraldPotion3 = new Healing("Great Health Potion", 15, emeraldBoss, 15);
+        itemRepository.save(sapphirePotion3);
+
+        Healing sapphirePotion4 = new Healing("Great Health Potion", 15, emeraldBoss, 15);
+        itemRepository.save(sapphirePotion4);
+
+        Item moneyBagEmerald = new Item("Basic Money Bag", 10, emeraldBoss);
+        itemRepository.save(moneyBagEmerald);
+
+        Item moneyBagEmerald2 = new Item("Average Money Bag", 15, emeraldBoss);
+        itemRepository.save(moneyBagEmerald2);
+
+        Item moneyBagEmerald3 = new Item("Average Money Bag", 15, emeraldBoss);
+        itemRepository.save(moneyBagEmerald3);
+
+        Item moneyBagEmerald4 = new Item("Great Money Bag", 20, emeraldBoss);
+        itemRepository.save(moneyBagEmerald4);
+
+
+
+        //DIAMOND ITEMS
+
         Item diamondGem = new Item("Diamond Gem", 0, diamondBoss);
         itemRepository.save(diamondGem);
+
+        Healing diamondPotion = new Healing("Average Health Potion", 10, diamondBoss, 10);
+        itemRepository.save(diamondPotion);
+
+        Healing diamondPotion2 = new Healing("Great Health Potion", 15, diamondBoss, 15);
+        itemRepository.save(sapphirePotion2);
+
+        Healing diamondPotion3 = new Healing("Great Health Potion", 15, diamondBoss, 15);
+        itemRepository.save(diamondPotion3);
+
+        Healing diamondPotion4 = new Healing("Great Health Potion", 15, diamondBoss, 15);
+        itemRepository.save(diamondPotion4);
+
+        Item moneyBagDiamond = new Item("Average Money Bag", 15, diamondBoss);
+        itemRepository.save(moneyBagDiamond);
+
+        Item moneyBagDiamond2 = new Item("Average Money Bag", 15, diamondBoss);
+        itemRepository.save(moneyBagDiamond2);
+
+        Item moneyBagDiamond3 = new Item("Average Money Bag", 15, diamondBoss);
+        itemRepository.save(moneyBagDiamond3);
+
+        Item moneyBagDiamond4 = new Item("Great Money Bag", 20, diamondBoss);
+        itemRepository.save(moneyBagDiamond4);
 
     }
 }
