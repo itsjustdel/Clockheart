071715f4e4ce7cd61ac2f9906ac6b15846161ff7
NaomiValuks <naomi@valuks.com>
2022-02-25 19:00:47 +0000

    added the book into components and the fontfamily into index.html

---
+++ ClockheartFrontEnd/app/src/components/Book.js
@@ -0,0 +1,54 @@
+import HTMLFlipBook from "react-pageflip"
+import './/Book.css'
+
+
+const Book = () => {
+    
+
+        return (
+          <HTMLFlipBook 
+          width={300}
+          height={400}
+          drawShadow={true}
+          maxShadowOpacity={0.5}
+          showCover={true}
+          id="book">
+            <div id="front-cover">
+            
+            </div>
+            <div id="inside-cover">
+            
+            </div>
+            <div id="page">
+            <p id="paragraphs">During the days before man and machine lived side by side, there was a group of individuals who set out to learn and discover more about how to interact with machines. </p>
+            <p id="paragraphs">They were known as <b>'The Developers'</b>. The stories of these individuals aren't known to be true, but from what historians believe, their real names were ...</p>
+            </div>
+            <div id="page">
+                <ul id="no-bullets">
+                    <li>Adam Kidd</li>
+                    <li>Del Reid</li>
+                    <li>Guilherme Nunes</li>
+                    <li>Jack Cogan</li>
+                    <li>Kirsten Buchan</li>
+                    <li>Marijke Drench</li>
+                    <li>Matthew McFarlane</li>
+                    <li>Noah Valuks</li>
+                </ul>
+            </div>
+            <div id="page">
+                <ul id="no-bullets">
+                    <li>Raminta Razauskaite</li>
+                    <li>Robbie Ferguson</li>
+                    <li>Ross Brown</li>
+                    <li>Sacha Ponniah</li>
+                    <li>Scott Reoch</li>
+                    <li>Tamer Amer</li>
+                    <li>Tony Fletcher</li>
+                </ul>
+            </div>
+          </HTMLFlipBook>
+        );
+      
+}
+
+export default Book
