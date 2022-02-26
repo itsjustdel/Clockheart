a6966602c199b857a99dd401d8c353ea5a1b94db
Ross Brown <rossbrown22@hotmail.co.uk>
2022-02-26 14:48:47 +0000

    book now renders on page

--- ClockheartFrontEnd/app/src/components/ClockTowerBar.js
+++ ClockheartFrontEnd/app/src/components/ClockTowerBar.js
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react'
 import Music from './Music'
 import Shop from './Shop'
 import QuestGiver from './QuestGiver'
-const ClockTowerBar= ({updatePlayerTarget, playerMesh, shopOpen, setShopOpen, questGiverOpen,setQuestGiverOpen}) => {
+import BookLocation from './BookLocation'
+const ClockTowerBar= ({updatePlayerTarget, playerMesh, shopOpen, setShopOpen, questGiverOpen, setQuestGiverOpen, bookLocationOpen, setBookLocationOpen}) => {
 
     useEffect( () => 
     {
@@ -17,6 +18,8 @@ const ClockTowerBar= ({updatePlayerTarget, playerMesh, shopOpen, setShopOpen, qu
             <Shop shopOpen={shopOpen} setShopOpen={setShopOpen} playerMesh={playerMesh} updatePlayerTarget={updatePlayerTarget}/>
 
             <QuestGiver questGiverOpen={questGiverOpen} setQuestGiverOpen={setQuestGiverOpen} playerMesh={playerMesh} updatePlayerTarget={updatePlayerTarget}/>
+
+            <BookLocation bookLocationOpen={bookLocationOpen} setBookLocationOpen={setBookLocationOpen} playerMesh={playerMesh} updatePlayerTarget={updatePlayerTarget}/>
 
             <Music url={"/SteampunkAmbience.mp3"} soundLevel={0.03}/>
             
--- ClockheartFrontEnd/app/src/containers/SceneManager.js
+++ ClockheartFrontEnd/app/src/containers/SceneManager.js
@@ -15,6 +15,8 @@ import Music from '../components/Music';
 import BossGUI from '../components/BossGUI';
 import CharacterCreationGUI from '../components/CharacterCreationGUI';
 import { getPlayerItems } from '../components/ItemServices';
+import BookLocation from '../components/BookLocation';
+import BookGUI from '../components/BookGUI';
 
 const SceneManager = () => {
 
@@ -29,8 +31,9 @@ const SceneManager = () => {
     const [questGiverOpen, setQuestGiverOpen] = useState(false)
     const [bossOpen, setBossOpen] = useState(false)
     const [characterCreationOpen, setCharacterCreationOpen] = useState(false)
+    const [bookLocationOpen, setBookLocationOpen] = useState(false)
 
-    const startLevel = { name: "Street" }
+    const startLevel = { name: "ClockTowerBar" }
     const [currentQuest, setCurrentQuest] = useState(startLevel)
     
 
@@ -102,7 +105,9 @@ const SceneManager = () => {
                 {currentQuest.name == "ClockTowerBar" ? 
                 <ClockTowerBar updatePlayerTarget={updatePlayerTarget} playerMesh={playerMesh}
                     shopOpen={shopOpen} setShopOpen={setShopOpen} questGiverOpen={questGiverOpen}
-                    setQuestGiverOpen={setQuestGiverOpen} setPlayerStartPosition={setPlayerStartPosition} /> 
+                    setQuestGiverOpen={setQuestGiverOpen} setPlayerStartPosition={setPlayerStartPosition}
+                    bookLocationOpen={bookLocationOpen} setBookLocationOpen={setBookLocationOpen}
+                     /> 
                 : null}
 
                 {currentQuest.name == "Rust and Dust" ? 
@@ -130,6 +135,7 @@ const SceneManager = () => {
 
             {characterCreationOpen == true ? <CharacterCreationGUI characters={characters} setCharacters={setCharacters} setCurrentQuest={setCurrentQuest} updateCharacters={updateCharacters} setCharacterCreationOpen={setCharacterCreationOpen} /> : null}
 
+            {bookLocationOpen == true ? <BookGUI setBookLocationOpen={setBookLocationOpen} /> : null}
         </>
     )
 }
