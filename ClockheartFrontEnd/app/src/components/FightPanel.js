9fc3a77c1e73e05eb219b6f4a6c0b81aa3c1f39d
Sacha Ponniah <sponniah812@gmail.com>
2022-02-26 20:54:57 +0000

    Can now use weapons or no item to attack and only healing items to heal. Using anything else for the worng purpose does nothing, but doesn't break

--- ClockheartFrontEnd/app/src/components/FightPanel.js
+++ ClockheartFrontEnd/app/src/components/FightPanel.js
@@ -46,20 +46,25 @@ const FightPanel = ({characters ,setCharacters, enemyId, items, setItems, select
 
     const attackClick = () => {
         console.log("enemy id  = " + enemy.id)
-        console.log("selected item damage: " + selectedItem.damage)
-        let attackStrength = selectedItem.damage
-
+        let attackStrength = 1
+        if(selectedItem !== null){
+            attackStrength = selectedItem.damage
+        }
+        
         //remove health form enemy by attack strength
         if(player.strength === 10){
             attackStrength += 5
         }
+        
+        console.log("attack strength: " + attackStrength)
 
-
-        if(enemy.healthPoints > attackStrength){
-            enemy.healthPoints -= attackStrength
-        }
-        else{
-            enemy.healthPoints = 0
+        if(attackStrength !== undefined){
+            if(enemy.healthPoints > attackStrength){
+                enemy.healthPoints -= attackStrength
+            }
+            else{
+                enemy.healthPoints = 0
+            }
         }
         //update characters in state with new character
         setCharacters(newCharacters)
@@ -85,9 +90,11 @@ const FightPanel = ({characters ,setCharacters, enemyId, items, setItems, select
         //Below will be set to the used item's healing value
         // const healing = 5;
         console.log("selected item healing: " + selectedItem.healing)
-        let healing = selectedItem.healing
-
-        const player = newCharacters[0]
+        let healing = 0
+        if(selectedItem !== null && selectedItem.healing !== undefined){
+            healing = selectedItem.healing
+        }
+    
         if (player.healthPoints < 100){
             player.healthPoints += healing
         }
