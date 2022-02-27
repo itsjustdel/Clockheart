561ae906a31df2e4919aa0390e1b8e4951c172b5
Del Reid <45520351+itsjustdel@users.noreply.github.com>
2022-02-27 17:18:56 +0000

    changed street level

--- ClockheartFrontEnd/app/src/containers/SceneManager.js
+++ ClockheartFrontEnd/app/src/containers/SceneManager.js
@@ -34,7 +34,7 @@ const SceneManager = () => {
     const [characterCreationOpen, setCharacterCreationOpen] = useState(false)
     const [bookLocationOpen, setBookLocationOpen] = useState(false)
 
-    const startLevel = { name: "Rust and Dust" }
+    const startLevel = { name: "Street" }
     const [currentQuest, setCurrentQuest] = useState(startLevel)
     const playerMesh = useRef()
 
