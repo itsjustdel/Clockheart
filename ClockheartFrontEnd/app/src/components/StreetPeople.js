266aa599352f49e6ac84df39248a93cdc0acc7b6
NaomiValuks <naomi@valuks.com>
2022-02-28 22:03:28 +0000

    changed streetpeople level

--- ClockheartFrontEnd/app/src/components/StreetPeople.js
+++ ClockheartFrontEnd/app/src/components/StreetPeople.js
@@ -5,36 +5,34 @@ const StreetPeople = () => {
 
     return(
         <>
-            <TexturedPlane url={["/people/blondeBlueHatSide.png"]}position={[-19, 5, 0]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/pinkHairFront.png"]}position={[-18.5, 5, -1.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/redHairSide.png"]}position={[-11.5, 5, -10]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/whiteBeardBlueTopFront.png"]}position={[-20.5, 5, -10.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/bubbleGumHairSide.png"]}position={[-22, 5, -8.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/aliceInWonderlandSide.png"]}position={[-19.5, 5, -9]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/silverHairSide.png"]}position={[-11.5, 5, -3.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/redHairSide.png"]}position={[-0.5, 5, 18.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/blueBack.png"]}position={[-2, 5, 19.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/blondeHairSide.png"]}position={[-3.5, 5, 18.7]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/purpleHairSide.png"]}position={[14, 5, 13.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/lightBlueHairBack.png"]}position={[-17.7, 5, 6]} args={[3, 3]}/>
-
-            <TexturedPlane url={["/people/purpleDressSide.png"]}position={[-17.7, 5, 9.5]} args={[3, 3]}/>
-
-            <TexturedPlane url={["/people/greenHairYellowTopFront.png"]}position={[-18.5, 5, 3.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/oldLadyGlassesSide.png"]}position={[-19.4, 5, 5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/blueHairFront.png"]}position={[0.7, 5, 12]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/greenHairBlueBowSide.png"]}position={[-0.2, 5, 13]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/blueHairSideClipSide.png"]}position={[2, 5, 12.7]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/pinkBonnetSide.png"]}position={[-11, 5, 8.8]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/yellowClothesFront.png"]}position={[-17.5, 5, 14]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/blondeHairSide.png"]}position={[-18, 5, 15.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/purpleLongHairSide.png"]}position={[-14.5, 5, 18.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/whiteOutfitSide.png"]}position={[-12.5, 5, 18]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/redPigTailsBack.png"]}position={[-17.5, 5, 17.5]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/blackHairSide.png"]}position={[-9.5, 5, 13]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/blueHairBack.png"]}position={[15, 5, 18]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/wiseManSide.png"]}position={[16, 5, 17]} args={[3, 3]}/>
-            <TexturedPlane url={["/people/whiteMaidHatFront.png"]}position={[14.8, 5, 15]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/blondeBlueHatSide.png"]}position={[-19, 4.5, 0]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/pinkHairFront.png"]}position={[-18.5, 4.5, -1.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/redHairSide.png"]}position={[-11.5, 4.5, -10]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/whiteBeardBlueTopFront.png"]}position={[-20.5, 4.5, -10.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/bubbleGumHairSide.png"]}position={[-22, 4.5, -8.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/aliceInWonderlandSide.png"]}position={[-19.5, 4.5, -9]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/silverHairSide.png"]}position={[-11.5, 4.5, -3.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/redHairSide.png"]}position={[-0.5, 4.5, 18.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/blueBack.png"]}position={[-2, 4.5, 19.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/blondeHairSide.png"]}position={[-3.5, 4.5, 18.7]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/purpleHairSide.png"]}position={[14, 4.5, 13.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/lightBlueHairBack.png"]}position={[-17.7, 4.5, 6]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/purpleDressSide.png"]}position={[-17.7, 4.5, 9.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/greenHairYellowTopFront.png"]}position={[-18.5, 4.5, 3.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/oldLadyGlassesSide.png"]}position={[-19.4, 4.5, 5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/blueHairFront.png"]}position={[0.7, 4.5, 12]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/greenHairBlueBowSide.png"]}position={[-0.2, 4.5, 13]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/blueHairSideClipSide.png"]}position={[2, 4.5, 12.7]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/pinkBonnetSide.png"]}position={[-11, 4.5, 8.8]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/yellowClothesFront.png"]}position={[-17.5, 4.5, 14]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/blondeHairSide.png"]}position={[-18, 4.5, 15.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/purpleLongHairSide.png"]}position={[-14.5, 4.5, 18.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/whiteOutfitSide.png"]}position={[-12.5, 4.5, 18]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/redPigTailsBack.png"]}position={[-17.5, 4.5, 17.5]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/blackHairSide.png"]}position={[-9.5, 4.5, 13]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/blueHairBack.png"]}position={[15, 4.5, 18]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/wiseManSide.png"]}position={[16, 4.5, 17]} args={[3, 3]}/>
+            <TexturedPlane url={["/people/whiteMaidHatFront.png"]}position={[14.8, 4.5, 15]} args={[3, 3]}/>
         </>
     )
 }
