47a100d19850d572b200bd58f3672e65830ea4b9
Del Reid <45520351+itsjustdel@users.noreply.github.com>
2022-03-01 21:42:57 +0000

    Update PlayerItemsGUI.js

--- ClockheartFrontEnd/app/src/components/GUI_Files/PlayerItemsGUI.js
+++ ClockheartFrontEnd/app/src/components/GUI_Files/PlayerItemsGUI.js
@@ -22,16 +22,18 @@ const PlayerItemsGUI = ({characters, items, setSelectedItem}) => {
         const filename = item.name.replace(/ /g, "_");
         if(item.character.id === 1)
             return <li className='playerItem' key={index}>
-                        <button onClick={handleSelectedItemClick} value={index}>{item.name}</button>
-                        <img className='playerItemImage' src={`/newPngs/${filename}.png`}/>
+                        {/* <button onClick={handleSelectedItemClick} value={index}>{item.name}</button> */}
+                        {/* <img className='playerItemImage' src={`/newPngs/${filename}.png`}/> */}
+
+                        {/* <div className ="shopItemImage">                             */}
+                            <input onClick={handleSelectedItemClick} value={index} type="image" src={`/newPngs/${filename}.png`} />
+                        {/* </div> */}
                         
                     </li>
     })
 
     return(
-        <>
-        <h2>Player Item List</h2>
-        {/* <p>Currency: {player.currency} </p> */}
+        <>        
             <ul className='playerItemList'>
                 {playerItems}
             </ul>
